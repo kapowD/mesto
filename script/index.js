45be35a67c229ca4f24035ab438b5adc599c060a
KapowD <mikael99988@gmail.com>
2023-04-16 08:21:52 +0700

    fix: hotfix review2

--- script/index.js
+++ script/index.js
@@ -1,26 +1,35 @@
 let editProfile= document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.pop-up');
 let editor = document.querySelector('.form');
-let inputName = document.querySelector('.pop-up__input_name');
-let inputDescription = document.querySelector('.pop-up__input_description');
+let inputName = document.querySelector('.pop-up__input_type_name');
+let inputDescription = document.querySelector('.pop-up__input_type_description');
 let popupSave = document.querySelector('.pop-up__edit')
 let profileDescription = document.querySelector('.profile__description');
 let profileName = document.querySelector('.profile__name');
 let popupClose = document.querySelector('.pop-up__exit');
 
+
+function openPopup(editPopup){
+    editPopup.classList.add('pop-up_opened');
+}
+
+function closePopup(editPopup){ 
+    editPopup.classList.remove('pop-up_opened');
+}
+
 editProfile.addEventListener('click', function () {
-    popup.classList.add('pop-up_opened');
+    openPopup(popup);
     inputName.value = profileName.textContent;
     inputDescription.value = profileDescription.textContent;
    
 })
 popupClose.addEventListener('click', function () {
-    popup.classList.remove('pop-up_opened');
+    closePopup(popup);
 })
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
-    popup.classList.remove('pop-up_opened');
+    closePopup(popup);
 }
 editor.addEventListener('submit', handleFormSubmit);
