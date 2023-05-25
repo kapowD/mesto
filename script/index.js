db1204b7fe22903d05a3ebc1b0ee51da430217d7
KapowD <mikael99988@gmail.com>
2023-05-25 16:26:35 +0700

    merge branch develop

--- script/index.js
+++ script/index.js
@@ -1,10 +1,11 @@
+import { initialCards } from './constants.js';
+
 const elementTemplate = document.getElementById('elements-tameplate').content;
 const elementsContainer = document.querySelector('.elements');
 
 const editProfile = document.querySelector('.profile__edit-button');
 const addCard = document.querySelector('.profile__add-button')
 
-// const popup = document.querySelector('.pop-up-edit');
 
 const popupEdit = document.querySelector('.pop-up-edit');
 const profileEditor = popupEdit.querySelector('.form-edit');
@@ -12,40 +13,34 @@ const inputName = popupEdit.querySelector('.pop-up__input_type_name');
 const inputDescription = popupEdit.querySelector('.pop-up__input_type_description');
 
 const popupAdd = document.querySelector('.pop-up-add');
-const addCardEditor = popupAdd.querySelector('.form-edit');
+const addCardEditor = popupAdd.querySelector('.form-add');
 const inputPlace = popupAdd.querySelector('.pop-up__input_type_place');
 const inputLink = popupAdd.querySelector('.pop-up__input_type_link');
+
+const popupImage = document.querySelector('.pop-up-image');
+const popupPhoto = document.querySelector('.pop-up__image');
+const popupSubtitle = document.querySelector('.pop-up__subtitle');
 
 const popupSave = document.querySelector('.pop-up__edit')
 const profileDescription = document.querySelector('.profile__description');
 const profileName = document.querySelector('.profile__name');
 
-const popupClose = document.querySelector('.pop-up__exit');
+const exitButton = document.querySelectorAll('.pop-up__exit');
 
-
-
-
-
-
-
-
-function openPopup(editPopup) {
-    editPopup.classList.add('pop-up_opened'); //общая функция открытия попапа
+exitButton.forEach((button) => {
+    const popup = button.closest('.pop-up');
+    button.addEventListener('click', () => closePopup(popup));
+});
+function openPopup(popup) {
+    popup.classList.add('pop-up_opened');
 }
-
-function closePopup(editPopup) {
-    editPopup.classList.remove('pop-up_opened');
+function closePopup(popup) {
+    popup.classList.remove('pop-up_opened');
 }
-
-
 editProfile.addEventListener('click', function () {
     openPopup(popupEdit);
     inputName.value = profileName.textContent;
     inputDescription.value = profileDescription.textContent;
-
-})
-popupClose.addEventListener('click', function () {
-    closePopup(popupEdit);
 })
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -55,43 +50,20 @@ function handleFormSubmit(evt) {
 }
 profileEditor.addEventListener('submit', handleFormSubmit);
 
-
-
-
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-//
 const createElement = (el) => {
     const cardElement = elementTemplate.querySelector('.element').cloneNode(true);
     const titleElement = cardElement.querySelector('.element__title')
-    const imageElement = cardElement.querySelector('.element__photo');
+    const imagePhoto = cardElement.querySelector('.element__photo');
     titleElement.textContent = el.name;
-    imageElement.src = el.link;
-    imageElement.alt = el.name;
+    imagePhoto.src = el.link;
+    imagePhoto.alt = el.name;
+    const openImage = cardElement.querySelector('.element__image-open').addEventListener('click', () => {
+        openPopup(popupImage);
+        popupPhoto.src = el.link;
+        popupPhoto.alt = el.link;
+        popupSubtitle.textContent = el.name;
+
+    })
     const deleteButton = cardElement.querySelector('.element__delete-button');
     const likeButton = cardElement.querySelector('.element__like-button');
     const handleDelete = () => {
@@ -104,23 +76,13 @@ const createElement = (el) => {
     likeButton.addEventListener('click', handleLike);
     return cardElement;
 };
-
-
-
 const renderCardElement = (cardElement) => {
     elementsContainer.prepend(cardElement);
 };
-
 initialCards.forEach((card) => {
     renderCardElement(createElement(card));
 });
-
-
-
-
-
 addCard.addEventListener('click', () => openPopup(popupAdd));
-
 function handleAddSubmit(event) {
     event.preventDefault();
     const name = inputPlace.value;
@@ -133,12 +95,4 @@ function handleAddSubmit(event) {
     closePopup(popupAdd);
     event.target.reset();
 };
-
-addCardEditor.addEventListener('submit', handleAddSubmit); // он опять ничего не видит и не принимает велью (кнопки закрытия я еще не прикрутил)
-
-
-
-
-
-
-
+addCardEditor.addEventListener('submit', handleAddSubmit); 
