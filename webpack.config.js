80db8415bf178f26b045e17896cb1b849198c5bb
KapowD <mikael99988@gmail.com>
2023-09-18 23:32:45 +0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        main: './script/index.js',
+        main: './src/script/index.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[hash].js'
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+    },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader',
+                ]
+            },
             {
                 test: /\.css$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
-                        loader: 'css-loader'
-                    }
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader',
+
                 ]
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/,
                 type: 'asset/resource',
-                generator:{
+                generator: {
                     filename: 'images/[name].[hash][ext]'
                 }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 type: 'asset/resource',
-                generator:{
+                generator: {
                     filename: 'fonts/[name].[hash][ext]'
                 }
             },
@@ -40,8 +57,9 @@ module.exports = {
     ,
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
+            template: './src/index.html',
         }),
         new MiniCssExtractPlugin(),
-    ]
+        new CleanWebpackPlugin(),
+    ],
 }
